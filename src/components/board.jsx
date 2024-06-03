d748ce05656b6742cd6d24d670e32dd6bbfc5191
Ethan Ramos <ethanramos@ethans-mbp-8.myfiosgateway.com>
2024-06-02 23:55:33 -0400

    Initial game features added

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,71 +1,67 @@
 import React from "react";
 import Tile from "./tile";
 import "../styles.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+const word = ["C", "O", "D", "E", "R"];
 const Board = () => {
   const [attempts, setAttempts] = useState(() =>
     Array(6)
       .fill(null)
-      .map(() => Array(5).fill(""))
+      .map(() => Array(5).fill({ letter: "", status: "empty" }))
   );
   const [currentRow, setCurrentRow] = useState(0);
   const [currentCell, setCurrentCell] = useState(0);
-
+  const eventHandlerRef = useRef();
+  eventHandlerRef.current = (event) => {
+    let updatedAttempts = attempts.map((row) =>
+      row.map((cell) => ({ ...cell }))
+    );
+    if (
+      event.key === "Enter" &&
+      !updatedAttempts[currentRow].map((curr) => curr.letter).includes("")
+    ) {
+      updatedAttempts[currentRow].forEach((row, rowIndex) => {
+        if (row.letter === word[rowIndex]) {
+          row.status = "correct";
+          console.log("correct");
+        } else if (word.includes(row.letter)) {
+          row.status = "wrongPosition";
+        } else {
+          row.status = "wrong";
+        }
+      });
+      setCurrentRow(currentRow + 1);
+      setCurrentCell(0);
+    } else if (event.key === "Backspace" && currentCell !== 0) {
+      updatedAttempts[currentRow][currentCell - 1] = "";
+      setCurrentCell(currentCell - 1);
+    } else if (/^[a-zA-Z]$/.test(event.key) && currentCell !== 5) {
+      updatedAttempts[currentRow][currentCell].letter = event.key.toUpperCase();
+      setCurrentCell(currentCell + 1);
+    }
+    setAttempts(updatedAttempts);
+    return;
+  };
   useEffect(() => {
-    console.log(attempts);
-    const handleKeyDown = (event) => {
-      if (event.key === "Enter") {
-        if (attempts[currentRow].includes("")) {
-          console.log("Please fill in all the blanks");
-          return;
-        } else {
-          setCurrentRow(currentRow + 1);
-          setCurrentCell(0);
-        }
-      } else if (event.key === "Backspace") {
-        if (currentCell === 0) {
-          console.log("You have reached the beginning of the row");
-          return;
-        } else {
-          setAttempts((prev) => {
-            const newAtempts = prev.map((row) => [...row]);
-            newAtempts[currentRow][currentCell - 1] = "";
-            return newAtempts;
-          });
-          setCurrentCell(currentCell - 1);
-        }
-      } else if (/^[a-zA-Z]$/.test(event.key)) {
-        if (currentCell === 5) {
-          return;
-        } else {
-          setAttempts((prev) => {
-            const newAtempts = [...prev];
-            newAtempts[currentRow][currentCell] = event.key;
-            return newAtempts;
-          });
-          setCurrentCell(currentCell + 1);
-        }
-      }
-    };
-
-    window.addEventListener("keydown", handleKeyDown);
+    const eventListener = (event) => eventHandlerRef.current(event);
+    window.addEventListener("keydown", eventListener);
     return () => {
-      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keydown", eventListener);
     };
-  }, [currentRow, attempts]);
+  }, []);
 
   return (
-    <>
-      <div id="gameboard">
-        {attempts.map((attempt, idx) => (
-          <div key={idx} className="row">
-            {attempt.map((letter, index) => (
-              <Tile key={index} letters={letter} />
-            ))}
-          </div>
-        ))}
-      </div>
-    </>
+    <div>
+      {attempts.map((row, rowIndex) => (
+        <div key={rowIndex} className="row">
+          {row.map((cell, cellIndex) => (
+            <div key={cellIndex} className={`cell cell-${cell.status}`}>
+              {cell.letter}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
   );
 };
 
